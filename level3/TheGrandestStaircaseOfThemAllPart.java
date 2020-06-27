5d4dc4fc5c281ed8e4f0633f04d2a96daef4c00e
Ankit Rauthan <ankitrauthan95@gmail.com>
2020-06-27 13:08:08 +0530

    Create TheGrandestStaircaseOfThemAllPart.java

---
+++ level3/TheGrandestStaircaseOfThemAllPart.java
@@ -0,0 +1,38 @@
+public class TheGrandestStaircaseOfThemAllPart
+{
+	public static void main(String[] args)
+	{
+		System.out.println(solution(9));
+	}
+	
+	public static int solution(int n) 
+    {
+		int[][] steps = new int[n+1][n+1];
+		
+		steps[0][0] = 1;
+		
+		for(int last=1; last <= n; last++)
+		{
+			 for (int left = 0; left  <= n; left ++) 
+	         {
+				 steps[last][left] = steps[last - 1][left];
+				 
+				 if(left >= last)
+				 {
+					 steps[last][left] += steps[last - 1][left - last];
+				 }
+			  }
+		}
+		
+		for(int i = 0; i<steps.length; i++)
+		{
+			for(int j = 0; j<steps[i].length; j++)
+			{
+				System.out.println("i: "+i+" and j is: "+j+" value is: "+steps[i][j]);
+			}
+		}
+		
+		return steps[n][n] - 1;
+    }
+
+}
